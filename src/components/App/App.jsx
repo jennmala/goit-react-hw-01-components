8245f1c18bbe07c7e76da6dba485d3ae1572235f
Yevheniia Malakhova <jen.mala.kh@gmail.com>
2022-03-17 20:10:23 +0200

    4th task

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,11 @@
 import { Profile } from 'components/Profile/Profile';
 import { Statistics } from 'components/Statistics/Statistics';
 import { FriendList } from 'components/FriendList/FriendList';
+import { TransactionHistory } from 'components/TransactionHistory/TransactionHistory';
 import user from 'user.json';
 import data from 'data.json';
 import friends from 'friends.json';
+import transactions from 'transactions.json';
 import { Container } from './App.styled';
 
 export const App = () => {
@@ -20,6 +22,8 @@ export const App = () => {
       <Statistics title="Upload stats" stats={data} />
 
       <FriendList friends={friends} />
+
+      <TransactionHistory items={transactions} />
     </Container>
   );
 };
--- src/components/FriendList/FriendList.styled.jsx
+++ src/components/FriendList/FriendList.styled.jsx
@@ -4,5 +4,6 @@ export const FriendListWrap = styled.ul`
   padding: 0;
   width: 400px;
   margin: 0 auto;
+  margin-bottom: 30px;
   list-style: none;
 `;
